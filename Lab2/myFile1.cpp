28808b53ffb38457d2e072652129d604314ac7ff
anaci032 <anaci032@uottawa.ca>
2024-10-06 16:07:01 -0400

    q1

---
+++ Lab2/myFile1.cpp
@@ -0,0 +1,26 @@
+/*code*/
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    int tab[10];
+    int *p;
+    for (int i = 0; i < 10; i++)
+    {
+        tab[i] = i * i;
+    }
+    // "*" fetches value of address, & fetches address of value, tab on its own is an address
+    cout << tab[2] << endl;
+    tab[2] = tab[1];
+    cout << tab[2] << endl;
+    tab[2] = *(tab + 1);
+    cout << tab[2] << endl;
+    *(tab + 2) = tab[1];
+    cout << tab[2] << endl;
+    *(tab + 2) = *(tab + 1);
+    cout << tab[2] << endl;
+    p = &tab[0];
+    p = tab + 1;
+    tab[4] = *p;
+    cout << tab[4] << endl;
+}
